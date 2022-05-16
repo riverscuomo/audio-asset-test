6f86ffb461ffa1e47c6b30a2bef71424a0e758d8
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2022-05-16 08:20:06 -0700

    ?

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'package:just_audio/just_audio.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final player = AudioPlayer();
-  // final audioSource = AudioSource.uri(Uri.parse(
-  //     "https://www.dropbox.com/s/e9crwcf9b4t1uyw/12%20Say%20It%20Ain%27t%20So.mp3?raw=1"));
-  // await player.setAudioSource(audioSource);
-  // await player.play();
-  // await player.setAsset("Say It Ain't So demo.mp3");
-  await player.setAsset("nature.mp3");
-  // player.play();
+  final _player = AudioPlayer();
+
+  // Try to load audio from a source and catch any errors.
+  try {
+    await _player.setAudioSource(AudioSource.uri(
+        // Uri.parse("https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3")));
+        Uri.parse("asset:///audio/nature.mp3")));
+
+    // await _player.setAsset("audio/nature.mp3");
+
+    _player.play();
+  } catch (e) {
+    print("Error loading audio source: $e");
+  }
 }
